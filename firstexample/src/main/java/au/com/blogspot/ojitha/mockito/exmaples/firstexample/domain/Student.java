1d25289ebf075162580b01f6c5a36d38db9ddb73
ojitha <ojithak@gmail.com>
2014-02-22 22:10:26 +1100

    removed everyting

--- firstexample/src/main/java/au/com/blogspot/ojitha/mockito/exmaples/firstexample/domain/Student.java
+++
@@ -1,5 +0,0 @@
-package au.com.blogspot.ojitha.mockito.exmaples.firstexample.domain;
-
-public class Student extends Person {
-
-}
--- firstexample/src/main/java/au/com/blogspot/ojitha/mockito/exmaples/firstexample/registration/Registration.java
+++
@@ -1,7 +0,0 @@
-package au.com.blogspot.ojitha.mockito.exmaples.firstexample.registration;
-
-import au.com.blogspot.ojitha.mockito.exmaples.firstexample.domain.Person;
-
-public interface Registration {
-	public boolean register(Person person);
-}
